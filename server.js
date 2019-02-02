ad29fce39d1442b5c382b41f6b0e8a6b19941425
Reginaldo <reginaldolribeiro@gmail.com>
2019-02-02 20:48:06 -0200

    Nodemon

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (request, response) => {
     console.log("Console log: Teste");
-    response.send("<h1>Welcome to the Jungle!</h1>");
+    response.send("<h1>Welcome to the Jungle!!!</h1>");
 });
 
 app.listen(3001);
