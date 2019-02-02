f51e63b5ed1ef505dbb76795be980e44a08113c8
Reginaldo <reginaldolribeiro@gmail.com>
2019-02-02 21:29:59 -0200

    MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Iniciando o app
 const app = express();
 
+// Iniciando o DB
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi", 
+    { useNewUrlParser: true }
+);
+//require('./src/models/Product');
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// Primeira rota
 app.get('/', (request, response) => {
+
+    Product.create({ 
+        title: 'React native',
+        description: 'Build native apps with Reactive',
+        url: "http://github.com/facebook/react-native"
+    });
+
     console.log("Console log: Teste");
-    response.send("<h1>Welcome to the Jungle!!!</h1>");
+    return response.send("<h1>Welcome to the Jungle!!!</h1>");
 });
 
 app.listen(3001);
