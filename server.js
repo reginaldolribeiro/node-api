973b90b192d65b1032029d71f4e645f78108b7dd
Reginaldo <reginaldolribeiro@gmail.com>
2019-02-02 20:41:57 -0200

    Commit inicial

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    console.log("Console log: Teste");
+    response.send("<h1>Welcome to the Jungle!</h1>");
+});
+
+app.listen(3001);
